a758522f45460e0eafe63d81bd4545a6b1c697c2
devejs <ssej0221@gmail.com>
2023-07-03 00:47:22 +0900

    commit 230701 TIL

---
+++ cpp_practice/interface.cpp
@@ -0,0 +1,12 @@
+#include "interface.h"
+
+void Interface::get_input()
+{
+    int a = 1;
+}
+
+int Interface::solution()
+{
+    printf("interface solution\n");
+    return 0;
+}
---
+++ cpp_practice/interface.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Interface
+{
+    public:
+        Interface() {};
+        ~Interface() {};
+
+        void get_input();
+        int solution();
+};
